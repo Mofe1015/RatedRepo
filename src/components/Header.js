3cce6169378fcbd9bc5fd46c55bca0567b0a17c1
mofe <mofeomotosho8@gmail.com>
2022-02-25 20:55:32 -0300

    project setup

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>working</h1>
+    </div>
+  );
+}
+
+export default Header;
