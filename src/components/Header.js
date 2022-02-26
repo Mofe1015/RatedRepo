a4803dfc09ca687da7639ceb373d2e5375e394b4
mofe <mofeomotosho8@gmail.com>
2022-02-25 23:25:31 -0300

    setting background image in navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,24 @@ import '../index.css';
 
 function Header() {
   return (
-    <div className="Header">
-      <h1>working</h1>
-    </div>
+    <section style={}>
+      
+    </section>
   );
 }
 
 export default Header;
+
+function changeBg(){
+  const images = [
+    'url(../images/bannerimg.jpg)',
+    'url(../images/bannerimg3.jpg)',
+    'url(../images/bannerimg2.jpg)',
+    'url(../images/bannerimg1.jpg)',
+
+  ]
+  const section = document.querySelector('section')
+  const bg = images[Math.floor(Math.random() * images.length )];
+  section.style.backgroundImage = bg;
+}
+setInterval(changeBg, 1000)
--- src/index.js
+++ src/index.js
@@ -13,3 +13,5 @@ ReactDOM.render(
   </>,
   document.getElementById('root')
 );
+
+
