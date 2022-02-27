9d30a4614d027f42a76e3544adc31683811f4e76
mofe <mofeomotosho8@gmail.com>
2022-02-27 12:22:16 -0300

    arranging compomnents and changing images

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,41 +5,56 @@ import bannerimg3 from'../images/bannerimg3.jpg';
 import bannerimg2 from'../images/bannerimg2.jpg';
 import bannerimg1 from'../images/bannerimg1.jpg';
 import bannerimg4 from'../images/movies.jpg';
+function TopNav(){
+  return(
+    <div className='navitemsdiv'>
+    <a href='#'>Write a Review</a>
+    <a href='#'>Events</a>
+    <a href='#'>Talks</a>
+    <a href='#'>Highly Rated</a>
+    <div className='navitemsdiv2'>
+      <a href='#'>Login </a>
+      <button>Register</button>
+    </div>
+  </div>
+  )
+}
 
-
-function Header() {
-  return (
-    <div className="Header">
-      <img src={require('../images/bannerimg5.jpg') }  className="bannerimg"></img>
-      <div className='innerbannerdiv'>
-        <div className='navitemsdiv'>
-          <a href='#'>Write a Review</a>
-          <a href='#'>Events</a>
-          <a href='#'>Talks</a>
-          <a href='#'>Highly Rated</a>
-          <div className='navitemsdiv2'>
-            <a href='#'>Login </a>
-            <button>Register</button>
-          </div>
-        </div>
-        <div className='searchbardiv'>
+function SearchBar(){
+  return(
+    <div className='searchbardiv'>
           <h1>Search</h1>
           <input  placeholder={"Restaurant, Movies, Gadgets, More ...."}></input>
-          <div class="vl"></div>
+          <div className='v1'></div>
           <h1>Location</h1>
           <input  placeholder={"Adrress, Street, City, State, Country "}></input>
           <button ><img src={require('../images/search.png') }  className="searchimg"/></button>
 
         </div>
-        
+  )
+}
 
+function HeaderItems(){
+  return(
+    <div className="Header">
+      <img src={require('../images/bannerimg8.jpg') }  className="bannerimg"></img>
+      <div className='innerbannerdiv'>
+        <TopNav/>
+       <SearchBar/>
       </div>
       
     </div>
-    
+  )
+}
+
+
+
+export default function Header() {
+  return (
+    <HeaderItems/>
   );
 }
 
-export default Header;
+
 
 
