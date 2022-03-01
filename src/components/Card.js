5871dc2127039c9b1914fffff690a54789cdd64a
mofe <mofeomotosho8@gmail.com>
2022-02-28 22:01:58 -0300

    adding btm section to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,10 @@ import '../index.css';
 function TopCardDiv(){
     return(
         <div className='itmimgdiv'>
-            <img className='itmimg' src={require('../images/itemimg2.jpg') } />
+             <a href='#'>
+                <img className='itmimg' src={require('../images/itemimg2.jpg') } />
+             </a>
+            
             
         </div>
     )
@@ -14,22 +17,45 @@ function TopCardDiv(){
 function BtmCardDiv(){
     return(
         <div className='btmcarddiv'>
-            
+            <div className='itemnameandlocationdiv'>
+               
+                <div className='itemname'>
+                    <img className='itemnameimg' src={require('../images/restaurant1.png') } />
+                    <a href='#' className='itemnametxt'>Pizza-Hut (Pepperoni Pizza)</a>
+                </div>
+                <div className='itemlocation'>
+                <img className='itemnameimg' src={require('../images/mapicon.png') } />
+                    <a href='#' className='itemnametxt'>1101 W Jackson, IL 60607</a>
+                </div>
+
+            </div>
             <div className='rateinfo'>
-                <h1 className='rateintotxt'>50k</h1>
+                <div className='rateintotxtdiv'>
+                    <h1 className='rateintotxt'>USA</h1>
+                </div>
                 <img src={require('../images/logocrop1.png') }  className="ratingsimg"></img>
-                <h1 className='rateintotxt1'>USA</h1>
+                <div className='rateintotxtdiv1'>
+                    <h1 className='rateintotxt1'>5k+</h1>
+                </div>
+                
             </div>
             
         </div>
     )
 };
+function FullCard(){
+    return(
+        <div className='fullcard'>
+            <TopCardDiv/>
+            <BtmCardDiv/>
+        </div>
+    )
+}
 
 function CardItems(){
     return(
         <div className='Hrateddiv'>
-            <TopCardDiv/>
-            <BtmCardDiv/>
+            <FullCard/>
         </div>
     )
 };
