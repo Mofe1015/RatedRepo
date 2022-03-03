346dbf156f73a1ae7e70372439f573969cce2816
mofe <mofeomotosho8@gmail.com>
2022-03-02 23:38:49 -0300

    adding scroll btn to changing scroll

--- src/components/Talks.js
+++ src/components/Talks.js
@@ -4,8 +4,17 @@ import '../index.css';
 
 
 function ChangingTalks(){
+    const leftscrollbtn = <button id="slideLeft" type="button" className='talksleftbtn'>
+        <img className='scrollimg' src={require('../images/arrowleft.png') } />
+    </button>
+    const rightscrollbtn =  <button id="slideRight" type="button" className='talksrightbtn'> 
+        <img className='scrollimg' src={require('../images/arrowright.png') } />
+    </button>
+
     return(
         <div className='changingtalks'>
+           {leftscrollbtn}
+           {rightscrollbtn}
 
         </div>
     )
