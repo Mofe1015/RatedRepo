09cb1065befdf4cef72a3b0d5d34027c5b6c3e1e
mofe <mofeomotosho8@gmail.com>
2022-03-03 00:09:16 -0300

    adding talks DB and rendering images to site

--- src/components/Talks.js
+++ src/components/Talks.js
@@ -1,29 +1,48 @@
 import React from 'react';
 import '../index.css';
+import TalksData from './TalksData';
 
 
+
+function TalksScrollInfo(props){
+    return(
+        <div>
+            <img className='talksimg' src={props.img} />
+        </div>
+    )
+}
+
+const AllTalks = TalksData.map((talksdata) => {
+    return(
+        <div>
+            <TalksScrollInfo
+                img = {talksdata.image}
+            />
+        </div>
+    )
+})
 
 function ChangingTalks(){
-    const leftscrollbtn = <button id="slideLeft" type="button" className='talksleftbtn'>
-        <img className='scrollimg' src={require('../images/arrowleft.png') } />
-    </button>
-    const rightscrollbtn =  <button id="slideRight" type="button" className='talksrightbtn'> 
-        <img className='scrollimg' src={require('../images/arrowright.png') } />
-    </button>
-
     return(
         <div className='changingtalks'>
-           {leftscrollbtn}
-           {rightscrollbtn}
-
+           {AllTalks}
         </div>
     )
     
 };
 
 function TalksTotal(){
+    const leftscrollbtn = <button id="talksslideLeft" type="button" className='talksleftbtn'>
+        <img className='scrollimg' src={require('../images/arrowleft.png') } />
+    </button>
+    const rightscrollbtn =  <button id="talksslideRight" type="button" className='talksrightbtn'> 
+        <img className='scrollimg' src={require('../images/arrowright.png') } />
+    </button>
+
     return(
         <div className='talkstotal'>
+            {leftscrollbtn}
+            {rightscrollbtn}
             <ChangingTalks/>
         </div>
     )
---
+++ src/components/TalksData.js
@@ -0,0 +1,13 @@
+import TalksImg from'../images/bannerimg11.jpg';
+
+
+export default [
+    {
+        id: 1,
+        image: TalksImg,
+    },
+    {
+        id: 2,
+        image: TalksImg,
+    },
+]
